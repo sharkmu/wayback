fc04174825bac37a798e99e75dcfa03b9c3d74eb
sharkmu <csonka.samuel@gmail.com>
2025-04-06 20:58:50 +0200

    stable version done, might have an error at the first try

---
+++ run.py
@@ -0,0 +1,35 @@
+import flask
+from waybackpy import WaybackMachineAvailabilityAPI
+import time
+
+app = flask.Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    start_time = time.time()
+    result = None
+    if flask.request.method == "POST":
+        url = flask.request.form.get("url")
+        user_agent = "Wayback Tool"
+        try:
+            availability_api = WaybackMachineAvailabilityAPI(url, user_agent)
+            oldest = availability_api.oldest()
+            latest = availability_api.archive_url
+            timestamp = availability_api.timestamp()
+            
+            result = {
+                "input_url": url,
+                "oldest_snapshot": oldest,
+                "latest_snapshot": latest,
+                "timestamp": timestamp.strftime("%Y-%m-%d %H:%M:%S")
+            }
+        except Exception as e:
+            print(f"Error: {e}")
+            result = False
+    
+    end_time = time.time()
+    action_time = round(end_time - start_time, 2)
+    return flask.render_template("index.html", result=result, action_time=action_time)
+
+if __name__ == "__main__":
+    app.run(debug=True)
